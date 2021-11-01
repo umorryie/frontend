898315a71e9340642e96a94f860bfaf9faa8977b
Matej Pesjak <matej.pesjak@equaleyes.com>
2021-11-01 23:58:13 +0100

    chor: Added image rendering and author and delete button

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,4 +1,9 @@
+import { HiOutlineDownload } from "react-icons/all";
+
+import { useState } from "react";
+
 import { ImagePayload } from "../../interfaces/imagePayload";
+import "./Card.css";
 
 export const Card = ({
   author,
@@ -8,5 +13,28 @@ export const Card = ({
   height,
   width,
 }: ImagePayload) => {
-  return <div>{author}</div>;
+  const [showFooter, setShowFooter] = useState(false);
+
+  return (
+    <div className="image-wrapper">
+      <img
+        src={download_url}
+        onMouseOver={() => setShowFooter(true)}
+        onMouseLeave={() => setShowFooter(false)}
+      />
+      {showFooter ? (
+        <div className="hover-footer">
+          <div className="author">{author}</div>
+          <a
+            href={download_url}
+            className="image-download"
+            download
+            target="_blank"
+          >
+            <HiOutlineDownload color="white" />
+          </a>
+        </div>
+      ) : null}
+    </div>
+  );
 };
--- frontend/src/components/CardList/CardList.tsx
+++ frontend/src/components/CardList/CardList.tsx
@@ -1,20 +1,52 @@
-import { ImagePayloadData } from "../../interfaces/imagePayload";
+import { useState, useEffect } from "react";
+
+import { ImagePayload, ImagePayloadData } from "../../interfaces/imagePayload";
 import { Card } from "../Card/Card";
+import "./CardList.css";
 
 export const CardList = ({ imageListPayloads }: ImagePayloadData) => {
-  const renderedCards = imageListPayloads.map((imagePayload, index) => {
+  const [xSize, setXSize] = useState(window.innerWidth);
+  const updateSize = () => setXSize(window.innerWidth);
+  useEffect(() => (window.onresize = updateSize), []);
+
+  const chuckedArray = chunkArray(
+    imageListPayloads,
+    imageListPayloads.length / (xSize < 1100 ? 2 : 3)
+  );
+  const renderedChunkedCards = chuckedArray.map((chunkedSubArray, index1) => {
     return (
-      <Card
-        key={index}
-        id={imagePayload.id}
-        author={imagePayload.author}
-        download_url={imagePayload.download_url}
-        url={imagePayload.url}
-        width={imagePayload.width}
-        height={imagePayload.height}
-      ></Card>
+      <div className="column" key={index1}>
+        {chunkedSubArray.map((imagePayload, index2) => {
+          return (
+            <Card
+              key={index2}
+              id={imagePayload.id}
+              author={imagePayload.author}
+              download_url={imagePayload.download_url}
+              url={imagePayload.url}
+              width={imagePayload.width}
+              height={imagePayload.height}
+            ></Card>
+          );
+        })}
+      </div>
     );
   });
 
-  return <div>{renderedCards}</div>;
+  return <div className="row">{renderedChunkedCards}</div>;
 };
+
+function chunkArray(
+  myArray: Array<ImagePayload>,
+  chunk_size: number
+): Array<Array<ImagePayload>> {
+  const arrayLength = myArray.length;
+  let tempArray = [];
+
+  for (let index = 0; index < arrayLength; index += chunk_size) {
+    const myChunk = myArray.slice(index, index + chunk_size);
+    tempArray.push(myChunk);
+  }
+
+  return tempArray;
+}
